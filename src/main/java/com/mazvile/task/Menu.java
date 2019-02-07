5890111fbbe95d56837c8c68a1d3c5d712f90e28
Mazvile <ab.mazvile@gmail.com>
2019-02-07 20:26:38 +0200

    Added Menu class with dummy methods

---
+++ src/main/java/com/mazvile/task/Menu.java
@@ -0,0 +1,21 @@
+package com.mazvile.task;
+
+import java.util.List;
+
+public class Menu {
+
+    public List<Recipe> makeRandomMenu(
+            int numberOfFishDishes,
+            int numberOfMeatDishes,
+            int numberOfChickenDishes,
+            int numberOfVegieDishes) {
+
+
+        return null;
+    }
+
+    public List<Recipe> getRecipesFromProductsInTheFridge() {
+
+        return null;
+    }
+}
